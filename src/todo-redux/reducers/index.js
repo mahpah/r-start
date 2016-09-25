a47c835222d31081b1826cf089795037b1f7693b
mahpah <mahpahh@gmail.com>
2016-09-25 11:27:38 +0700

    refactor(s4): remove filter reducer
    Why: router now is single source of trust

--- src/todo-redux/reducers/index.js
+++ src/todo-redux/reducers/index.js
@@ -1,19 +1,8 @@
 import { combineReducers } from 'redux';
 import { todos, getVisibleTodos as getTodos } from './todos';
 
-const visibilityFilter = (state = 'SHOW_ALL', action) => {
-  const { type, payload } = action;
-
-  if (type === 'SET_FILTER') {
-    return payload;
-  }
-
-  return state;
-};
-
 export const todoApp = combineReducers({
   todos,
-  visibilityFilter,
 });
 
 export const getVisibleTodos = (state, filter) => getTodos(state.todos, filter);
