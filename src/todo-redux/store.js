b5e59a8855a5113be6de28babf91f4af15619795
mahpah <mahpahh@gmail.com>
2016-09-25 13:13:14 +0700

    feat(s4): add logging

--- src/todo-redux/store.js
+++ src/todo-redux/store.js
@@ -5,7 +5,22 @@ import throttle from 'lodash/throttle';
 
 const persistedState = loadState() || undefined;
 
+const addLog = (store) => {
+  const origDispatch = store.dispatch;
+
+  return (action) => {
+    console.group(action.type);
+    console.log('Before', store.getState());
+    console.log('Action', action);
+    const returnValue = origDispatch(action);
+    console.log('After', store.getState());
+    console.groupEnd(action.type);
+    return returnValue;
+  };
+};
+
 export const store = createStore(todoApp, persistedState);
+store.dispatch = addLog(store);
 
 store.subscribe(throttle(() => {
   saveState(store.getState());
